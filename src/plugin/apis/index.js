c0a4c6b0baba5aeeeda3a1df25dda65432224363
Jaimi Patel <jaimip.albiorix@gmail.com>
2025-02-27 16:55:15 +0530

    Add axios, apis and alert configuration

---
+++ src/plugin/apis/index.js
@@ -0,0 +1,15 @@
+const requests = {
+  signUp: 'auth/sign-up',
+  login: 'auth/login',
+
+  getUsers: 'users/get-user-list',
+  getProducts: 'product/get-product-list',
+
+  addCart: 'cart/add-to-cart',
+  getCart: 'cart/get-cart',
+  deleteCart: 'cart/delete-cart',
+  deleteCartItem: 'cart/delete-cart-item/',
+  getCartCount: 'cart/get-cart-count',
+}
+
+export default requests
